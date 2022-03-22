5051ddecb288b9931bb589fda39432003ccdd855
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-03-22 20:18:36 -0300

    add postgres connection

---
+++ venhapararecomb/infra/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+
+uri = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:123456@localhost:5432/postgres")
+
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
+    uri
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    except:
+        db.close()

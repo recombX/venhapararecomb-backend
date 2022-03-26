564dd77443a3e459fcdb549ffc66d8fd31483b0b
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-03-26 18:07:45 -0300

    update env settings

--- app/infra/settings.py
+++ app/infra/settings.py
@@ -5,12 +5,12 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     # app envs
     app_name: str = "FastApi"
-    app_docker_run: str
-    app_test: str
+    app_docker_run: str | None = None
+    app_test: str | None = None
     dev: str
     # database envs
     database_url: str
-    database_url_docker: str
+    database_url_docker: str | None = None
 
     class Config:
         env_file = ".env"

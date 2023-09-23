94621f5913b0357c4085fe73447de2d021abaf15
Maciel-Dev <jvmacielv@gmail.com>
2023-09-23 15:24:43 -0300

    Adição do Docker

--- front-end/recombfront/src/App.vue
+++ front-end/recombfront/src/App.vue
@@ -4,7 +4,6 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template class="">
 <!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
-
   <RouterView />
 </template>
 
--- front-end/recombfront/src/service/axiosService.js
+++ front-end/recombfront/src/service/axiosService.js
@@ -13,13 +13,13 @@ const URL_API_FILE_CLIENT = "https://e7f4-2804-1b3-9402-40e8-a8d7-47e6-38c0-95f7
 export async function sendFileXML(file)
 {
     const config = { headers: { 'Content-Type': 'multipart/form-data', "ngrok-skip-browser-warning": "69420" } };
-    return axios.post(URL_API_FILE_NGROK, file, config)
+    return axios.post(URL_API_FILE, file, config)
 }
 
 export async function searchClient(value)
 {
     const config = { headers: { "ngrok-skip-browser-warning": "69420" } };
-    return axios.get(URL_API_FILE_CLIENT + "/" + value, config)
+    return axios.get(URL_API_CLIENT + "/" + value, config)
 }
 
 export async function teste()

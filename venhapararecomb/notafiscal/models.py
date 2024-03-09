3b55e45b26f2450ed1bdd78259e5097932ec5849
jcquadros <julliepaper@gmail.com>
2024-03-09 18:44:56 -0300

    gerenciamento de nf e estilizacao index.html concluidos

--- venhapararecomb/notafiscal/models.py
+++ venhapararecomb/notafiscal/models.py
@@ -1,20 +1,42 @@
+from typing import Any
 from django.db import models
 
 class Fornecedor(models.Model):
     nome = models.CharField(max_length=100)
     cnpj = models.CharField(max_length=14)
 
-class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
-    identificador = models.CharField(max_length=14) # cpf ou cnpj
+    def __str__(self) -> str:
+        return self.nome
+
+class NotaFiscal(models.Model):
     fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'Nota Fiscal {self.fornecedor.nome}'
 
 class Boleto(models.Model):
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     data_vencimento = models.DateField()
-    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
+    nota_fiscal = models.ForeignKey(NotaFiscal, on_delete=models.CASCADE)
 
-class NotaFiscal(models.Model):
-    fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
-    clientes = models.ManyToManyField(Cliente)
-    boletos = models.ManyToManyField(Boleto)
+    def __str__(self) -> str:
+        return f'Boleto {self.valor} - {self.data_vencimento}'
+
+class Cliente(models.Model):
+    TIPO_DOCUMENTO = (
+        ('CPF', 'CPF'),
+        ('CNPJ', 'CNPJ'),
+    )
+
+    nome = models.CharField(max_length=100)
+    tipo_documento = models.CharField(max_length=4, choices=TIPO_DOCUMENTO)
+    documento = models.CharField(max_length=14) # cpf ou cnpj
+    nota_fiscal = models.ForeignKey(NotaFiscal, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.nome
+
+    
+
+
+   
--- venhapararecomb/notafiscal/views.py
+++ venhapararecomb/notafiscal/views.py
@@ -1,65 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import xml.etree.ElementTree as ET
-from django.shortcuts import redirect
 from .models import Fornecedor, Cliente, Boleto, NotaFiscal
-import json
+
+
+def parse_xml(xml_file):
+    xml = ET.parse(xml_file)
+    root = xml.getroot()
+    nsNFe = {'ns': 'http://www.portalfiscal.inf.br/nfe'}
+
+    xNome = root.find('ns:NFe/ns:infNFe/ns:emit/ns:xNome', nsNFe).text
+    cnpj = root.find('ns:NFe/ns:infNFe/ns:emit/ns:CNPJ', nsNFe).text
+    fornecedor = {'nome': xNome, 'cnpj': cnpj}
+
+    clientes = []
+    for dest in root.findall('ns:NFe/ns:infNFe/ns:dest', nsNFe):
+        xNome = dest.find('ns:xNome', nsNFe).text
+        cpf = dest.find('ns:CPF', nsNFe)
+        cnpj = dest.find('ns:CNPJ', nsNFe)
+
+        documento = cpf.text if cpf is not None else cnpj.text
+        tipo_documento = 'CPF' if cpf is not None else 'CNPJ'
+
+        clientes.append({'nome': xNome, 'documento': documento, 'tipo_documento': tipo_documento})
+
+    boletos = []
+    for det in root.findall('ns:NFe/ns:infNFe/ns:cobr/ns:dup', nsNFe):
+        valor = det.find('ns:vDup', nsNFe).text
+        data_vencimento = det.find('ns:dVenc', nsNFe).text
+        boletos.append({'valor': valor, 'data_vencimento': data_vencimento})
+
+    return {'fornecedor': fornecedor, 'clientes': clientes, 'boletos': boletos}
 
 
 def index(request):
-    xml_data = {}
-    
+    context = {}
+
     if request.method == 'POST':
         if 'read_xml' in request.POST:
-            xml = ET.parse(request.FILES['xml_file'])
-            root = xml.getroot()
-            nsNFe = {'ns': 'http://www.portalfiscal.inf.br/nfe'}
-            # recuperando o nome e o cnpj do fornecedor
-            xNome = root.find('ns:NFe/ns:infNFe/ns:emit/ns:xNome', nsNFe).text
-            cnpj = root.find('ns:NFe/ns:infNFe/ns:emit/ns:CNPJ', nsNFe).text
-            #fornecedor, created = Fornecedor.objects.get_or_create(nome=xNome, cnpj=cnpj)
-            fornecedor = {'nome': xNome, 'cnpj': cnpj}
-            
-            # recuperando o nome e o cpf ou cnpj dos clientes
-            clientes = []
-            for dest in root.findall('ns:NFe/ns:infNFe/ns:dest', nsNFe):
-                xNome = dest.find('ns:xNome', nsNFe).text
-                cpf = dest.find('ns:CPF', nsNFe)
-                cnpj = dest.find('ns:CNPJ', nsNFe)
-                if cpf is not None:
-                    ident = cpf.text
-                else:
-                    ident = cnpj.text
-                clientes.append({'nome': xNome, 'identificador': ident})
-            
-                #cliente, created = Cliente.objects.get_or_create(nome=xNome, identificador=ident, fornecedor=fornecedor)
-            boletos = []
-            for det in root.findall('ns:NFe/ns:infNFe/ns:cobr/ns:dup', nsNFe):
-                valor = det.find('ns:vDup', nsNFe).text
-                data_vencimento = det.find('ns:dVenc', nsNFe).text
-                boletos.append({'valor': valor, 'data_vencimento': data_vencimento})
-                #boleto = Boleto.objects.create(valor=valor, data_vencimento=data_vencimento, fornecedor=fornecedor)
-                xml_data = {'fornecedor': fornecedor, 'clientes': clientes, 'boletos': boletos}
-                request.session['xml_data'] = xml_data
+            xml_data = parse_xml(request.FILES['xml_file'])
+            request.session['xml_data'] = xml_data
+            context['xml_data'] = xml_data
 
         elif 'save_nf' in request.POST:
-            xml_data = request.session.get('xml_data', {})
-           
-            fornecedor, created = Fornecedor.objects.get_or_create(nome=xml_data['fornecedor']['nome'], cnpj=xml_data['fornecedor']['cnpj'])
-            
+            xml_data = request.session.pop('xml_data', {})
+
+            fornecedor, _ = Fornecedor.objects.get_or_create(nome=xml_data['fornecedor']['nome'], cnpj=xml_data['fornecedor']['cnpj'])
+            nf = NotaFiscal.objects.create(fornecedor=fornecedor)
+
             for cliente in xml_data['clientes']:
-                Cliente.objects.create(nome=cliente['nome'], identificador=cliente['identificador'], fornecedor=fornecedor)
+                Cliente.objects.create(nome=cliente['nome'], documento=cliente['documento'], tipo_documento=cliente['tipo_documento'], nota_fiscal=nf)
+
             for boleto in xml_data['boletos']:
-                Boleto.objects.create(valor=boleto['valor'], data_vencimento=boleto['data_vencimento'], fornecedor=fornecedor)
-            
-            nf=NotaFiscal.objects.create(fornecedor=fornecedor)
-            nf.clientes.set(Cliente.objects.filter(fornecedor=fornecedor))
-            nf.boletos.set(Boleto.objects.filter(fornecedor=fornecedor))
-
+                Boleto.objects.create(valor=boleto['valor'], data_vencimento=boleto['data_vencimento'], nota_fiscal=nf)
+          
             return redirect('index')
-            
-    return render(request, 'index.html', {'xml_data': xml_data})
 
-
-
-
-
+    return render(request, 'index.html', context)
